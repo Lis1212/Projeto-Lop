6e2754c981ae89f7dc347188a323d654253fcf42
Elis <eliscamara4@gmail.com>
2026-04-26 12:29:59 -0300

    Alteração no código completo

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
 const carro = document.querySelector("#carro");
-const cameraRig = document.querySelector("#camera");
+const camera = document.querySelector("#camera");
 
-document.addEventListener("keydown", function(e){
 
-let pos = carro.getAttribute("position");
+// parar
+let velocidade = 0;
+let angulo = 0;
+const aceleracao = 0.015;
+const forcaGiro = 2.0;
+const atrito = 0.96; 
 
-// frente
-if(e.key === "w"){
-    pos.z -= 1;
+const teclas = {};
+
+// monitorar as teclas
+document.addEventListener("keydown", (e) => teclas[e.key.toLowerCase()] = true);
+document.addEventListener("keyup", (e) => teclas[e.key.toLowerCase()] = false);
+
+function atualizar() {
+    // entrada (botoẽs)
+    if (teclas['w']) velocidade -= aceleracao; //frente
+    if (teclas['s']) velocidade += aceleracao; //trás 
+    
+    // girar o carro 
+    if (Math.abs(velocidade) > 0.001) {
+        if (teclas['a']) angulo += forcaGiro;
+        if (teclas['d']) angulo -= forcaGiro;
+    }
+
+    // atrito
+    velocidade *= atrito;
+
+    // posição atual
+    let pos = carro.getAttribute("position");
+
+    // cálculo de direção (conversão de grau por radiano)
+    let rad = angulo * (Math.PI / 180);
+
+    // X e Z baseados no ângulo
+    pos.x += Math.sin(rad) * velocidade;
+    pos.z += Math.cos(rad) * velocidade;
+
+    // aplica as novas transformações ao carro
+    carro.setAttribute("position", pos);
+    carro.setAttribute("rotation", { x: 0, y: angulo, z: 0 });
+
+    // câmera seguindo o carro
+    camera.setAttribute("position", {
+        x: pos.x,
+        y: 6,
+        z: pos.z + 12
+    });
+
+    requestAnimationFrame(atualizar);
 }
 
-// tras
-if(e.key === "s"){
-    pos.z += 1;
-}
-
-// esquerda
-if(e.key === "a"){
-    pos.x -= 0.8;
-}
-
-// direita
-if(e.key === "d"){
-    pos.x += 0.8;
-}
-
-carro.setAttribute("position", pos);
-
-});
-
-/* câmera */
-function atualizarCamera(){
-
-let pos = carro.getAttribute("position");
-
-cameraRig.setAttribute("position", {
-x: pos.x,
-y: 6,
-z: pos.z + 14
-});
-
-cameraRig.setAttribute("rotation", {
-x: 0,
-y: 0,
-z: 0
-});
-
-requestAnimationFrame(atualizarCamera);
-
-}
-
-atualizarCamera();
+// inicia
+atualizar();
