70be598d47b13d3952c203943c8058f39691e006
Elis <eliscamara4@gmail.com>
2026-04-26 13:02:31 -0300

    Revisão completa

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const carro = document.querySelector("#carro");
-const camera = document.querySelector("#camera");
+const cameraRig = document.querySelector("#cameraRig");
 
-
-// parar
 let velocidade = 0;
 let angulo = 0;
 const aceleracao = 0.015;
 const forcaGiro = 2.0;
-const atrito = 0.96; 
+const atrito = 0.96; // parar
 
 const teclas = {};
 
-// monitorar as teclas
+// monitoramento de teclas
 document.addEventListener("keydown", (e) => teclas[e.key.toLowerCase()] = true);
 document.addEventListener("keyup", (e) => teclas[e.key.toLowerCase()] = false);
 
 function atualizar() {
-    // entrada (botoẽs)
-    if (teclas['w']) velocidade -= aceleracao; //frente
-    if (teclas['s']) velocidade += aceleracao; //trás 
+    // lógica de entrada
+    if (teclas['w']) velocidade -= aceleracao; // frente
+    if (teclas['s']) velocidade += aceleracao; // trás 
     
-    // girar o carro 
+    // gira o carro
     if (Math.abs(velocidade) > 0.001) {
         if (teclas['a']) angulo += forcaGiro;
         if (teclas['d']) angulo -= forcaGiro;
@@ -32,10 +30,10 @@ function atualizar() {
     // posição atual
     let pos = carro.getAttribute("position");
 
-    // cálculo de direção (conversão de grau por radiano)
+    // cálculo de direção (convertido de graus lara medianos)
     let rad = angulo * (Math.PI / 180);
 
-    // X e Z baseados no ângulo
+    // Atualiza X e Z baseados no ângulo
     pos.x += Math.sin(rad) * velocidade;
     pos.z += Math.cos(rad) * velocidade;
 
@@ -43,8 +41,8 @@ function atualizar() {
     carro.setAttribute("position", pos);
     carro.setAttribute("rotation", { x: 0, y: angulo, z: 0 });
 
-    // câmera seguindo o carro
-    camera.setAttribute("position", {
+    // Câmera 
+    cameraRig.setAttribute("position", {
         x: pos.x,
         y: 6,
         z: pos.z + 12
@@ -53,5 +51,5 @@ function atualizar() {
     requestAnimationFrame(atualizar);
 }
 
-// inicia
+// incia
 atualizar();
